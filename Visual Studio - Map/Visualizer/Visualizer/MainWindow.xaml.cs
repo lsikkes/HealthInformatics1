39474b07198e679d4b9b8e4b89e17d0be0113fb8
Maiko <M.Goudriaan-2@student.tudelft.nl>
2016-04-29 17:13:14 +0200

    removed inline commets

--- Visual Studio - Map/Visualizer/Visualizer/MainWindow.xaml.cs
+++ Visual Studio - Map/Visualizer/Visualizer/MainWindow.xaml.cs
@@ -25,7 +25,6 @@ namespace GeenNaam
         
         public MainWindow()
         {
-            // Initialize the environment
             InitializeComponent();
 
             Uri iconUri = new Uri(AbsolutePath + "logo.PNG");
@@ -38,7 +37,6 @@ namespace GeenNaam
 
             Console.WriteLine("window size is " + width + "x" + height);
 
-            // Fill the environment with objects
             setPlayer((int)Math.Round(0.55 * width), (int)Math.Round(0.35 * height), 230);
             addSurrounding();
             addCharacters();
@@ -51,41 +49,40 @@ namespace GeenNaam
         // method to add all the surroundings to the environment
         public void addSurrounding()
         {
-            // prepare the map
             map.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
             map.VerticalAlignment = System.Windows.VerticalAlignment.Top;
             Thickness margin = map.Margin;
             margin.Left = 0;
             margin.Top = 0;
             map.Margin = margin;
-
             Brush color;
-            // add walls to the the environment
             color = Brushes.LightGray;
             addSquare(50, 1080, 0, 0, color);
             addSquare(500, 200, 300, 0, color);
             addSquare(500, 200, 1000, 0, color);
-            addSquare(1200, 200, 300, 700, color);
-            // add trees to the environment
-            color = Brushes.LightGreen;
+            addSquare(1200, 200, 300, 700, color);         
+        }
+
+        // adds trees to the environment
+        public void addTrees()
+        {
+            Brush color = Brushes.LightGreen;
             addSquare(100, 100, 400, 590, color);
             addSquare(100, 100, 550, 590, color);
         }
+
         // method to place all seats
         public void addSeats()
         {
-            // Add bench #1
-            Brush color;
-            color = Brushes.Brown;
+            Brush color = Brushes.Brown;
             addSquare(210, 70, 1100, 620, color);
             addSeat(1110, 630);
             addSeat(1180, 630);
             addSeat(1250, 630);
-            // Add more benches
         }
 
         //method to add a square to the environment
-        private void addSquare(int w, int h, int Mx, int My, System.Windows.Media.Brush color)
+        private void addSquare(int w, int h, int mx, int my, System.Windows.Media.Brush color)
         {
             System.Windows.Shapes.Rectangle rect;
             rect = new System.Windows.Shapes.Rectangle();
@@ -97,8 +94,8 @@ namespace GeenNaam
             rect.Height = h;
 
             Thickness margin = rect.Margin;
-            margin.Left = Mx;
-            margin.Top = My;
+            margin.Left = mx;
+            margin.Top = my;
             rect.Margin = margin;
             rect.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
             rect.VerticalAlignment = System.Windows.VerticalAlignment.Top;
@@ -109,7 +106,6 @@ namespace GeenNaam
         // method to set the player within the map
         public void setPlayer(int x, int y, int rotation)
         {
-            // Create a StackPanel to contain the shape.
             StackPanel panel;
             panel = new StackPanel();
             panel.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
@@ -118,7 +114,7 @@ namespace GeenNaam
             margin.Left = x;
             margin.Top = y;
             panel.Margin = margin;
-            // add the scoope
+
             Image Mole = new Image();
             Mole.Width = 400;
             Mole.Height = 400;
@@ -128,7 +124,7 @@ namespace GeenNaam
             ImageSource MoleImage = new BitmapImage(new Uri(ImgNameMole));
             Mole.Source = MoleImage;
             panel.Children.Add(Mole);
-            // add the image
+
             Mole = new Image();
             Mole.Width = 50;
             Mole.Height = 50;
@@ -140,10 +136,9 @@ namespace GeenNaam
             margin.Top = -410;
             Mole.Margin = margin;
             panel.Children.Add(Mole);
-            // apply the rotation
+
             RotateTransform rotateTransform1 = new RotateTransform(rotation, 10, 190);
             panel.RenderTransform = rotateTransform1;
-            // append
             map.Children.Add(panel);
         }
 
@@ -153,10 +148,10 @@ namespace GeenNaam
             addCharacter(1000, 300, 0, 0);
             addCharacter(800, 300, 1, 1);
         }
+
         //method to add a character
         public void addCharacter(int x, int y, int ID, int emoji)
         {
-            // Create a StackPanel to contain the shape.
             StackPanel panel;
             panel = new StackPanel();
             panel.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
@@ -165,7 +160,7 @@ namespace GeenNaam
             margin.Left = x;
             margin.Top = y;
             panel.Margin = margin;
-            // add the image
+
             StackPanel panel2;
             panel2 = new StackPanel();
             panel2.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
@@ -186,7 +181,7 @@ namespace GeenNaam
 
             panel2.Children.Add(Mole);
             panel.Children.Add(panel2);
-            // add the emoji
+
             Mole = new Image();
             Mole.Width = 25;
             Mole.Height = 25;
@@ -200,7 +195,7 @@ namespace GeenNaam
             margin.Top = -105;
             Mole.Margin = margin;
             panel.Children.Add(Mole);
-            // append
+
             map.Children.Add(panel);
         }
 
@@ -210,10 +205,10 @@ namespace GeenNaam
             addLight(800, 50);
             addLight(950, 50);
         }
+
         // Method to add a light to the environment
         public void addLight(int x, int y)
         {
-            // Create a StackPanel to contain the shape.
             StackPanel panel;
             panel = new StackPanel();
             panel.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
@@ -222,7 +217,7 @@ namespace GeenNaam
             margin.Left = x;
             margin.Top = y;
             panel.Margin = margin;
-            // add the image
+
             Image Mole = new Image();
             Mole.Width = 50;
             Mole.Height = 50;
@@ -231,7 +226,7 @@ namespace GeenNaam
             Mole.Source = MoleImage;
 
             panel.Children.Add(Mole);
-            // append
+
             map.Children.Add(panel);
         }
 
@@ -240,10 +235,10 @@ namespace GeenNaam
         {
             addTV(500, 200);
         }
+
         // Method to add a light to the environment
         public void addTV(int x, int y)
         {
-            // Create a StackPanel to contain the shape.
             StackPanel panel;
             panel = new StackPanel();
             panel.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
@@ -252,14 +247,14 @@ namespace GeenNaam
             margin.Left = x;
             margin.Top = y;
             panel.Margin = margin;
-            // add the image
+
             Image Mole = new Image();
             Mole.Width = 50;
             Mole.Height = 50;
             String location = AbsolutePath + "icon_tv.PNG";
             ImageSource MoleImage = new BitmapImage(new Uri(location));
             Mole.Source = MoleImage;
-            // append
+
             panel.Children.Add(Mole);
             map.Children.Add(panel);
         }
@@ -269,10 +264,10 @@ namespace GeenNaam
         {
             addCar(150, 20, 0);
         }
+
         // method to set a car within the map
         public void addCar(int x, int y, int rotation)
         {
-            // Create a StackPanel to contain the shape.
             StackPanel panel;
             panel = new StackPanel();
             panel.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
@@ -281,7 +276,7 @@ namespace GeenNaam
             margin.Left = x;
             margin.Top = y;
             panel.Margin = margin;
-            // add the scoope
+
             Image Mole = new Image();
             Mole.Width = 125;
             Mole.Height = 250;
@@ -289,7 +284,7 @@ namespace GeenNaam
             ImageSource MoleImage = new BitmapImage(new Uri(ImgNameMole));
             Mole.Source = MoleImage;
             panel.Children.Add(Mole);
-            // add the image
+
             Mole = new Image();
             Mole.Width = 50;
             Mole.Height = 50;
@@ -301,17 +296,16 @@ namespace GeenNaam
             margin.Top = -300;
             Mole.Margin = margin;
             panel.Children.Add(Mole);
-            // apply the rotation
+
             RotateTransform rotateTransform1 = new RotateTransform(rotation, 65, 125);
             panel.RenderTransform = rotateTransform1;
-            // append
+
             map.Children.Add(panel);
         }
 
         // Method to add a light to the environment
         public void addSeat(int x, int y)
         {
-            // Create a StackPanel to contain the shape.
             StackPanel panel;
             panel = new StackPanel();
             panel.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
@@ -320,14 +314,14 @@ namespace GeenNaam
             margin.Left = x;
             margin.Top = y;
             panel.Margin = margin;
-            // add the image
+
             Image Mole = new Image();
             Mole.Width = 50;
             Mole.Height = 50;
             String location = AbsolutePath + "icon_chair.PNG";
             ImageSource MoleImage = new BitmapImage(new Uri(location));
             Mole.Source = MoleImage;
-            // append
+
             panel.Children.Add(Mole);
             map.Children.Add(panel);
         }
