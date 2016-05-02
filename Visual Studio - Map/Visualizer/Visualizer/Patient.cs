e602800bf5e40c727fbe76d94f664996bcb49298
lsikkes <louis-sikkes@live.nl>
2016-05-02 14:33:04 +0200

    Moved player movement to Patient class

--- Visual Studio - Map/Visualizer/Visualizer/Patient.cs
+++ Visual Studio - Map/Visualizer/Visualizer/Patient.cs
@@ -20,6 +20,28 @@ namespace Visualizer
      * */
     public class Patient : VRObject
     {
-        public Patient()        {        }
+        public Patient()        {     }
+
+        public void walkUp()
+        {
+            walk(0, -50);
+            RenderTransform = new RotateTransform(270, 10, 190);
+        }
+
+        public void walkDown()
+        {
+            walk(0, 50);
+            RenderTransform = new RotateTransform(90, 10, 190);
+        }
+
+        public void walkRight(){
+            walk(50, 0);
+            RenderTransform = new RotateTransform(0, 10, 190);
+        }
+
+        public void walkLeft(){
+            walk(-50, 0);
+            RenderTransform = new RotateTransform(180, 10, 190);
+        }
     }
 }
