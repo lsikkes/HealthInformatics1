9ffb2c46fe999ba7e24932200f9a9b37162f7618
Maiko <M.Goudriaan-2@student.tudelft.nl>
2016-05-11 11:00:18 +0200

    initial test commit

---
+++ Visual Studio - Map/Visualizer/VisualizerTest/Class1.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VisualizerTest
+{
+    [TestFixture]
+    public class Class1
+    {
+        #region Methods
+
+        [Test]
+        public void helloWorld()
+        {
+            Assert.True(true);
+        }
+
+        #endregion Methods
+    }
+}
