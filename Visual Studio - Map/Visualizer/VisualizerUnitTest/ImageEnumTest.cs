9a710257c43b6601c1423e719d964ba36538e87d
Maiko <M.Goudriaan-2@student.tudelft.nl>
2016-05-18 09:53:34 +0200

    added the real test file

---
+++ Visual Studio - Map/Visualizer/VisualizerUnitTest/ImageEnumTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Visualizer;
+
+namespace VisualizerUnitTest
+{
+    [TestFixture]
+    public class ImageEnumTest
+    {
+        #region Methods
+
+        [Test]
+        public void GetImageTest()
+        {
+            ImageMap.loadImages();
+            Assert.NotNull(ImageMap.getImage(ImageEnum.PoliceCar.ToString()));
+        }
+
+        #endregion Methods
+    }
+}
